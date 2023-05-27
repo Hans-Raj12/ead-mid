e0d7043c524401ff3541170026436f1288e2ae45
Hans Raj <75563900+Hans-Raj12@users.noreply.github.com>
2023-05-27 17:14:36 +0500

    Add PollDisplay message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Timer from './task_b/components/Timer';
+import PollDisplay from './task_a/components/PollDisplay';
 function App() {
   return (
     <div className="App">
+    {/* Task->A */}
+    <PollDisplay/>
+
+
+
       {/* Task-B */}
-      <Timer initial={10}/>
+      {/* <Timer initial={10}/> */}
     </div>
   );
 }
---
+++ src/task_a/components/PollDisplay.js
@@ -0,0 +1,64 @@
+import React, {useEffect, useLayoutEffect, useState} from 'react'
+import './style.css'
+const poll_questions = [
+    {
+
+        "question": "What is your favorite programming language?",
+        "choices": [         
+          { "id": 1, "label": "JavaScript", "votes": 0 },         
+          { "id": 2, "label": "Python", "votes": 0 },
+          { "id": 3, "label": "Java", "votes": 0 },
+          { "id": 4, "label": "C#", "votes": 0 }
+        ]
+    }
+]
+export default function PollDisplay() {
+
+    const [question, setQuestion] = useState({
+
+        "question": "What is your favorite programming language?",
+        "choices": [         
+          { "id": 1, "label": "JavaScript", "votes": 0 },         
+          { "id": 2, "label": "Python", "votes": 0 },
+          { "id": 3, "label": "Java", "votes": 0 },
+          { "id": 4, "label": "C#", "votes": 0 }
+        ]
+    })
+    const [vote, setVote] = useState()
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e.target)
+    }
+
+      
+  return (
+     <>   
+        <form  name='votes-form'>
+            <h3>{`1. ${question.question}`}</h3>
+            <fieldset id='group1' name="choices">
+            {question.choices&& question.choices.map((choice)=>{
+                    return(
+                        <div className='choice'>
+                            <label htmlFor={choice.label}>{`${choice.id}. ${choice.label}`}</label>
+                        </div>
+                    )
+            })}
+            </fieldset>
+    </form>
+    <div>
+        <h1>Votes</h1>
+            {question.choices&& question.choices.map((choice)=>{
+                    return(
+                        <div className='votes'>
+                            <p>{`${choice.id}. ${choice.label}:`}</p>
+                            <p>{`${choice.votes} votes`}</p>
+                        </div>
+                    )
+            })}
+    </div>
+
+
+    </>
+  )
+}
